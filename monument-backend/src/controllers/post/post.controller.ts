c86dd9c6b9ac4b0f8b6fca179b0e26d4813636f5
Kucznsky <michal.kuczynski2000@wp.pl>
2023-10-28 19:31:04 +0200

    created post controller

---
+++ monument-backend/src/controllers/post/post.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Body, Post } from '@nestjs/common';
+import { CreatePostResponseDto } from 'src/core/dtos/create-post-response.dto/create-post-response.dto';
+import { PostDto } from 'src/core/dtos/post.dto/post.dto';
+import { PostFactoryService } from 'src/use_cases/post.use-case/post-factory/post-factory.service';
+import { PostUseCase } from 'src/use_cases/post.use-case/post.use-case';
+
+@Controller('post')
+export class PostController {
+    constructor(
+        private postServices: PostUseCase,
+        private postFactoryService: PostFactoryService,
+      ) {}
+
+    @Post()
+    async createPost(@Body() postDto: PostDto) {
+        const createdPostResponse = new CreatePostResponseDto();
+        try {
+            const post = this.postFactoryService.createPost(postDto)
+            const createdPost = await this.postServices.createPost(post)
+
+            createdPostResponse.createdPost = createdPost;
+            createdPostResponse.success = true;
+        } catch (error) {
+            createdPostResponse.success = false;
+        }
+
+        return createdPostResponse;
+    }
+}
