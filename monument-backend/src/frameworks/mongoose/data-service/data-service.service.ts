3bd6f22419a40a91aa22878cb8cb856f94896c00
Kucznsky <michal.kuczynski2000@wp.pl>
2023-11-01 16:50:35 +0100

    changes in data service

--- monument-backend/src/frameworks/mongoose/data-service/data-service.service.ts
+++ monument-backend/src/frameworks/mongoose/data-service/data-service.service.ts
@@ -1,5 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { IDataServices } from 'src/core/abstracts/data-service.abstract';
+import { User, UserDocument } from '../models/user/user.model';
+import { Post, PostDocument } from '../models/post/post.model';
+import { MongoGenericRepository } from '../mongo-generic-repository/mongo-generic-repository';
+import { InjectModel } from '@nestjs/mongoose';
+import { Like, LikeDocument } from '../models/like/like.model';
+import { Model } from 'mongoose';
+import { Comment, CommentDocument } from '../models/comment/comment.model';
 
 @Injectable()
-export class DataService  {
+export class DataService implements IDataServices, OnApplicationBootstrap  {
+    users: MongoGenericRepository<User>;
+    posts: MongoGenericRepository<Post>;
+    comments: MongoGenericRepository<Comment>;
+    likes: MongoGenericRepository<Like>;
+  
+    constructor(
+      @InjectModel(User.name)
+      private UserRepository: Model<UserDocument>,
+      @InjectModel(Post.name)
+      private PostRepository: Model<PostDocument>,
+      @InjectModel(Comment.name)
+      private CommentRepository: Model<CommentDocument>,
+      @InjectModel(Like.name)
+      private LikeRepository: Model<LikeDocument>,
+    ) {}
+
+    onApplicationBootstrap() {
+        this.users = new MongoGenericRepository<User>(this.UserRepository);
+        this.posts = new MongoGenericRepository<Post>(this.PostRepository);
+        this.comments = new MongoGenericRepository<Comment>(this.CommentRepository);
+        this.likes = new MongoGenericRepository<Like>(this.LikeRepository);
+      }
 }
