124f6c5226046a86f19d8eb7b816deab6f49605e
Kucznsky <michal.kuczynski2000@wp.pl>
2023-11-11 21:55:52 +0100

    added id generating in postFactoryService

--- monument-backend/src/use_cases/post/post-factory/post-factory.service.ts
+++ monument-backend/src/use_cases/post/post-factory/post-factory.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PostDto } from 'src/core/dtos/post.dto/post.dto';
 import { Post } from 'src/core/entities/post.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class PostFactoryService {
     public createPost(postDto: PostDto): Post{
         const post = new Post();
+        post.id = uuidv4();
         post.title = postDto.title;
-        // post.author = postDto.authorId;
+        post.author = postDto.authorId;
         post.category = postDto.category;
         post.content = postDto.content;
         post.localisation = postDto.localisation;
-        post.published = postDto.published
+        post.published = new Date(Date.now())
         return post
     }
 }
