b55a3fdb898f9859ffb496380e28e1f0116c1c8a
Kucznsky <michal.kuczynski2000@wp.pl>
2023-11-18 23:43:48 +0100

    added logic to register part of auth controller

--- monument-backend/src/controllers/auth/auth.controller.ts
+++ monument-backend/src/controllers/auth/auth.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthDto } from 'src/core/dtos/auth/auth.dto';
 import { AuthUseCase } from 'src/use_cases/auth/auth.use-case';
+import { UserFactoryService } from 'src/use_cases/user/user-factory/user-factory.service';
+import { UserUseCase } from 'src/use_cases/user/user-use-case/user.use-case';
+import * as argon from 'argon2'
+import { CreateUserResponseDto } from 'src/core/dtos/auth/created-user-response.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(
-        private authService: AuthUseCase,
+        private readonly authService: AuthUseCase,
+        private readonly userService: UserUseCase,
+        private readonly userFactoryService: UserFactoryService
       ) {}
 
     @Post('login')
@@ -13,7 +20,18 @@ export class AuthController {
     };
 
     @Post('register')
-    register(){
-        return this.authService.register()
+    async register(@Body() authDto: AuthDto){
+        const hash = await argon.hash(authDto.password);
+        const createdUserResponse = new CreateUserResponseDto()
+        try{
+            const newUser = this.userFactoryService.createUserObject(authDto, hash)
+            const createdUser = await this.userService.register(newUser)
+            createdUserResponse.createdUser = createdUser
+            createdUserResponse.success = true;
+        } catch(error){
+            console.log(error)
+            createdUserResponse.success = false;
+        }
+        return createdUserResponse;
     };
 }
