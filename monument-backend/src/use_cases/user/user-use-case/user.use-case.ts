bf3ea8d11819744f3204b978ad2924eb667b99eb
Kucznsky <michal.kuczynski2000@wp.pl>
2023-11-19 19:41:52 +0100

    created first parto of signing in logic

--- monument-backend/src/use_cases/user/user-use-case/user.use-case.ts
+++ monument-backend/src/use_cases/user/user-use-case/user.use-case.ts
@@ -1,6 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { IDataServices } from "src/core/abstracts/data-service.abstract";
+import { LoginDto } from "src/core/dtos/auth/login.dto";
 import { User } from "src/core/entities/user.entity";
+import * as argon from 'argon2'
+
 
 @Injectable()
 
@@ -9,12 +12,25 @@ export class UserUseCase {
         private dataServices: IDataServices,
       ) {}
 
-    register(user: User) {
+    async register(user: User) {
         try {
             const createdPost = this.dataServices.users.create(user)
             return createdPost
         } catch (error) {
             throw error
         }
+    };
+
+    async login(loginDto: LoginDto): Promise<User> {
+        const user = await this.dataServices.users.getByEmail(loginDto.email)
+        console.log(user)
+        if(!user) {
+            throw new ForbiddenException(`There's no user with this email`); 
+        }
+        const arePasswordsMatching = await argon.verify(user.passwordHash, loginDto.password)
+        if(!arePasswordsMatching){
+            throw new ForbiddenException(`Incorrect password`); 
+        }
+        return user;
     };
 }
