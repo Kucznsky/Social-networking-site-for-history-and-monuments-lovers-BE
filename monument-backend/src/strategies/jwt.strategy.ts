de3671c58169b28c20448f7786b0a0470c64bb8b
Kucznsky <michal.kuczynski2000@wp.pl>
2023-11-24 21:17:21 +0100

    added validate function to the JwtStrategy

--- monument-backend/src/strategies/jwt.strategy.ts
+++ monument-backend/src/strategies/jwt.strategy.ts
@@ -12,4 +12,9 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
             secretOrKey: config.get('JWT_SECRET')
         })
     }
+
+    validate(payload: any) {
+        console.log(payload)
+        return payload;
+    }
 }
