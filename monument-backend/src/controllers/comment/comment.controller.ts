71621fcd03172169177d822618bcb6a0da9047da
Kucznsky <michal.kuczynski2000@wp.pl>
2023-11-25 01:19:06 +0100

    comment controller - work in progress

--- monument-backend/src/controllers/comment/comment.controller.ts
+++ monument-backend/src/controllers/comment/comment.controller.ts
@@ -1,6 +1,58 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { CommentResponseDto } from 'src/core/dtos/comment/comment-response.dto';
+import { CommentDto } from 'src/core/dtos/comment/comment.dto';
+import { CommentFactoryService } from 'src/use_cases/comment/comment-factory/comment-factory.service';
+import { CommentUseCase } from 'src/use_cases/comment/comment-use-case/comment.use-case';
 
 @Controller('comment')
-@UseGuards(AuthGuard('jwt'))
-export class CommentController {}
+export class CommentController {
+    constructor(
+        private commentService: CommentUseCase,
+        private commentFactoryService: CommentFactoryService,
+      ) {}
+
+    @Post()
+    @UseGuards(AuthGuard('jwt'))
+    async createComment(@Body() commentDto: CommentDto) {
+        const createdCommentResponse = new CommentResponseDto();
+        try {
+            const comment = this.commentFactoryService.createCommentObject(commentDto);
+            const createdComment = await this.commentService.createComment(comment);
+
+            createdCommentResponse.createdComment = createdComment;
+            createdCommentResponse.success = true;
+        } catch (error) {
+            createdCommentResponse.success = false;
+        }
+
+        return createdCommentResponse;
+    }
+
+    @Put(':commentId')
+    @UseGuards(AuthGuard('jwt'))
+    async editComment(@Param('commentId') commentId: string, @Body() commentDto: CommentDto) {
+        const editedCommentResponse = new CommentResponseDto();
+        try {
+            const comment = this.commentFactoryService.createCommentObject(commentDto);
+            const editedComment = await this.commentService.editComment(commentId, comment);
+
+            editedCommentResponse.createdComment = editedComment;
+            editedCommentResponse.success = true;
+        } catch (error) {
+            editedCommentResponse.success = false;
+        }
+        return editedCommentResponse;
+    }
+
+    @Get(':postId')
+    getComments(){
+        //TODO
+    }
+
+    @Delete(':id')
+    @UseGuards(AuthGuard('jwt'))
+    async deleteComment(@Param('id') commentId: string) {
+        return this.commentService.deleteComment(commentId)
+    }
+}
