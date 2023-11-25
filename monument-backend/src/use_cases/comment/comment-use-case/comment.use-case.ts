3fd81c35213766cbb9d2704d9bc1570625b873ef
Kucznsky <michal.kuczynski2000@wp.pl>
2023-11-25 01:17:07 +0100

    started creating comment use cases

--- monument-backend/src/use_cases/comment/comment-use-case/comment.use-case.ts
+++ monument-backend/src/use_cases/comment/comment-use-case/comment.use-case.ts
@@ -1,5 +1,26 @@
 import { Injectable } from "@nestjs/common";
+import { Comment } from "src/core/entities/comment.entity";
 
 @Injectable()
 
-export class CommentUseCase {}
+export class CommentUseCase {
+    createComment(comment: Comment): Promise<Comment>{
+        //TODO
+        return
+    }
+
+    editComment(commentId: string, comment: Comment): Promise<Comment> {
+        //TODO
+        return
+    }
+
+    getComments(commentId: string){
+        //TODO
+        return
+    }
+
+    deleteComment(commentId: string) {
+        //TODO
+        return
+    }
+}
