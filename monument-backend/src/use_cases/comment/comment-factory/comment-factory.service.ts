d1fb9a86a6fc47741e4e652efaac9a9f51a534d4
Kucznsky <michal.kuczynski2000@wp.pl>
2023-11-25 01:18:40 +0100

    implemented createCommentObject in commentFactoryService

--- monument-backend/src/use_cases/comment/comment-factory/comment-factory.service.ts
+++ monument-backend/src/use_cases/comment/comment-factory/comment-factory.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CommentDto } from 'src/core/dtos/comment/comment.dto';
+import { Comment } from 'src/core/entities/comment.entity';
 
 @Injectable()
-export class CommentFactoryService {}
+export class CommentFactoryService {
+    createCommentObject(commentDto: CommentDto): Comment{
+        const comment = new Comment();
+        comment.author = commentDto.authorId;
+        comment.content = commentDto.content;
+        comment.post = commentDto.postId;
+
+        return comment
+    }
+}
