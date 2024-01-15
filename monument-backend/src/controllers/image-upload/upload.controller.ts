d6f18e53041dadfc5e5c5b8bf7119f800d9d7bba
Kucznsky <michal.kuczynski2000@wp.pl>
2024-01-15 22:38:42 +0100

    added new endpoints

--- monument-backend/src/controllers/image-upload/upload.controller.ts
+++ monument-backend/src/controllers/image-upload/upload.controller.ts
@@ -4,21 +4,37 @@ import { ImageUploadService } from 'src/use_cases/image-upload/image-upload/imag
 
 @Controller('upload')
 export class UploadController {
-    constructor(private readonly imageUploadService: ImageUploadService) {}
+  constructor(private readonly imageUploadService: ImageUploadService) {}
 
-    @Post()
-    @UseInterceptors(FileInterceptor('file'))
-    async uploadFile(
-        @UploadedFile(
-          new ParseFilePipe({
-            validators: [
-              new MaxFileSizeValidator({ maxSize: 1000000 }),
-              new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
-            ],
-          }),
-        )
-        file: Express.Multer.File,
-      ) {
-        await this.imageUploadService.uploadFile(file.originalname, file.buffer);
-      }
+  @Post('/user-avatar')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadUserAvatar(
+      @UploadedFile(
+        new ParseFilePipe({
+          validators: [
+            new MaxFileSizeValidator({ maxSize: 1000000 }),
+            new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+          ],
+        }),
+      )
+      file: Express.Multer.File,
+    ) {
+      await this.imageUploadService.uploadFile(file.originalname, file.buffer);
+    }
+
+  @Post('/post-thumbnail')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadThumbnail(
+      @UploadedFile(
+        new ParseFilePipe({
+          validators: [
+            new MaxFileSizeValidator({ maxSize: 1000000 }),
+            new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+          ],
+        }),
+      )
+      file: Express.Multer.File,
+    ) {
+      await this.imageUploadService.uploadFile(file.originalname, file.buffer);
+    }
 }
