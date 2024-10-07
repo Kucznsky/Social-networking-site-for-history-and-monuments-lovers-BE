72ab1f5c1e345d4509d8375d161290eaf1e393d3
Kucznsky <michal.kuczynski2000@wp.pl>
2024-10-07 13:20:58 +0200

    added swagger configuration to the project

--- monument-backend/src/main.ts
+++ monument-backend/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true}))
   app.enableCors();
+
+  const config = new DocumentBuilder()
+  .setTitle('Monument API')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'swagger/json',});
+
   await app.listen(3000);
 }
 bootstrap();
